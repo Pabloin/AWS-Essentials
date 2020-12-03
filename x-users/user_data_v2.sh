afe3449e596d35de2b5c45ee7c5b3ae009b1adfb
Pabloin <pablo.inchausti.bf@gmail.com>
2020-12-03 00:51:22 -0300

    Create user_data_v2.sh

---
+++ x-users/user_data_v2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum install python3 python3-pip python3-setuptools -y
+python3 -m pip install flask
+python3 -m pip install flask
+wget https://s3.amazonaws.com/...../flask_website.zip
+unzip flask_website.zip
+./start.py
+
+
+# start.py
+# #! /usr/bin/env python3
+# 
+# import webserver
