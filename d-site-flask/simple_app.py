108fa9ab4863259f70b3435f21c0662577b602e1
Pabloin <pablo.inchausti.bf@gmail.com>
2020-12-17 02:17:24 -0300

    Python Minisite Flask

--- d-site-flask/simple_app.py
+++ d-site-flask/simple_app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "Hello World"
+    return 'Hello World'
 
-@app.route('/temp', method=['GET', 'POST'])
+@app.route('/temp', methods=['GET', 'POST'])
 def temperature():
-    temp = request.args.get("temp")
-    return "temp " + temp
+    temp = request.args.get('temp')
+    return 'temp ' + temp
 
 if __name__ == '__main__':
     app.run()
