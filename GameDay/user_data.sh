a978036642165b297a309ace8d9382d3c3ad4334
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2023-10-11 11:34:33 -0300

    Add GameDay

--- GameDay/user_data.sh
+++ GameDay/user_data.sh
@@ -5,10 +5,23 @@ yum install git -y
 service httpd start
 chkconfig httpd on
 
+# Hello World web Site
+
 cd /var/www/html
 echo "Hello!" > index.html
+
+
+# Static web Site
 
 git clone https://github.com/Pabloin/AWS-Essentials.git
 
 cp -r AWS-Essentials/GameDay/game-day-site/* /var/www/html/
+
+
+# Dynamic web Site
+
+sudo apt-get install php
+
+echo "<?php echo '<p>Hello World</p>'; ?>" > /var/www/html/index.php
+
 
