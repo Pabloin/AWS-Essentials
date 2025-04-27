e4e4995c7dce40c32b3a61f2746d40617eeaee16
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2025-04-27 19:48:06 -0300

    Update GlaciarApp - v2025

--- 03-AWS-Services/Readme.md
+++ 03-AWS-Services/Readme.md
@@ -9,3 +9,14 @@
 - Hello-world ... 
 
         Todo: está todavía en WIP
+
+
+## Commands
+
+
+### Install Byobu 
+
+cat /etc/os-release
+sudo amazon-linux-extras install epel -y
+sudo yum install -y byobu 
+sudo yum install -y tree
---
+++ 03-AWS-Services/labs-elasticbeanstalk/hello-world-app/src/main/java/io/glaciar/elbns/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
