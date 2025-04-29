6282b97929e441cd153e1c1bbe136fa128a8298e
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2025-04-29 00:28:59 -0300

    Update GlaciarApp - Terraform Labs - v2025

--- 03-AWS-Services/labs-terraform/Tf-lab-02/modules/s3_bucket/main.tf
+++ 03-AWS-Services/labs-terraform/Tf-lab-02/modules/s3_bucket/main.tf
@@ -1,4 +1,8 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-terraform-bucket-${var.env_name}"
+resource "aws_s3_bucket" "glaciar" {
+  bucket = "io-glaciar-tf-${var.env_name}"
+}
+
+resource "aws_s3_bucket_acl" "glaciar" {
+  bucket = aws_s3_bucket.glaciar.id
   acl    = "private"
 }
--- 03-AWS-Services/labs-terraform/Tf-lab-02/modules/s3_bucket/outputs.tf
+++ 03-AWS-Services/labs-terraform/Tf-lab-02/modules/s3_bucket/outputs.tf
@@ -1,3 +1,3 @@
 output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
+  value = aws_s3_bucket.glaciar.bucket
 }
