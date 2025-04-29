1afab91c0e2f48275657a6347e9eacb3964ea399
Pabloin <pablo.ezequiel.inchausti@gmail.com>
2025-04-29 00:39:33 -0300

    Update GlaciarApp - Terraform Labs - v2025

--- 03-AWS-Services/labs-terraform/Tf-lab-02/modules/s3_bucket/main.tf
+++ 03-AWS-Services/labs-terraform/Tf-lab-02/modules/s3_bucket/main.tf
@@ -1,8 +1,14 @@
 resource "aws_s3_bucket" "glaciar" {
   bucket = "io-glaciar-tf-${var.env_name}"
+  force_destroy  = true
 }
 
-resource "aws_s3_bucket_acl" "glaciar" {
+resource "aws_s3_bucket_public_access_block" "glaciar" {
   bucket = aws_s3_bucket.glaciar.id
-  acl    = "private"
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
+
